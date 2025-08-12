b51b994bc64205576944b44bf71e479b1231c4e4
mohit <mohitchandola970@gmail.com>
2025-08-12 11:06:27 +0530

    Create multiple changes using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { HomePage } from "./Pages/HomePage"
+import { Routes, Route } from "react-router"
 
 function App() {
   return (
-    <>
-      <HomePage/>
-    </>
-  )
+    <Routes>
+      <Route index element={<HomePage />}/>
+      <Route path="checkout" element={<div>hello</div>}/>
+    </Routes> 
+  );
 }
 
 export default App
