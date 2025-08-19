26d3a9c366a736a03971f309f2df5ed4c7446f64
mohit <mohitchandola970@gmail.com>
2025-08-19 11:33:45 +0530

    use axios to fretch the data

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,13 @@
+import axios from 'axios'
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 
 export function HomePage() {
-     fetch("http://localhost:3000/api/products")
+     axios.get("http://localhost:3000/api/products")
        .then((res) => {
-         return res.json();
-       }).then((data) => {
-        console.log(data);
-        
+        console.log(res.data); 
        });
   return (
     <>
