bb5de2ae39595445921d2a910c0c20784a52118c
priyansh-09 <priyanshuchandola9@gmail.com>
2025-08-26 16:41:15 +0530

    create delete cart item feature

--- src/Pages/CartItemsDetails.jsx
+++ src/Pages/CartItemsDetails.jsx
@@ -1,36 +1,44 @@
 import { formatMoney } from "./checkout/utils/money";
 import DeliveryOptions from "./checkout/DeliveryOptions";
+import axios from "axios";
 
 export default function CartItemsDetails({cartitem, deliveryoptions, loadCart}) {
-  return (
-<>
-<div className="cart-item-details-grid">
-            <img className="product-image" src={cartitem.product.image} />
 
-            <div className="cart-item-details">
-                <div className="product-name">{cartitem.product.name}</div>
-                <div className="product-price">
-                {formatMoney(cartitem.product.priceCents)}
-                </div>
-                <div className="product-quantity">
-                <span>
-                    Quantity:{" "}
-                    <span className="quantity-label">
-                    {cartitem.quantity}
-                    </span>
-                </span>
-                <span className="update-quantity-link link-primary">
-                    Update
-                </span>
-                <span className="delete-quantity-link link-primary">
-                    Delete
-                </span>
-                </div>
-            </div>
-                
-            <DeliveryOptions cartitem={cartitem} deliveryoptions={deliveryoptions} loadCart={loadCart} />
-            
-            </div>
+  const  deleteItem = async()=>{
+    await axios.delete(`/api/cart-items/${cartitem.productId}`);
+    loadCart();
+  }
+  return (
+    <>
+      <div className="cart-item-details-grid">
+        <img className="product-image" src={cartitem.product.image} />
+
+        <div className="cart-item-details">
+          <div className="product-name">{cartitem.product.name}</div>
+          <div className="product-price">
+            {formatMoney(cartitem.product.priceCents)}
+          </div>
+          <div className="product-quantity">
+            <span>
+              Quantity:{" "}
+              <span className="quantity-label">{cartitem.quantity}</span>
+            </span>
+            <span className="update-quantity-link link-primary">Update</span>
+            <span
+              className="delete-quantity-link link-primary"
+              onClick={deleteItem}
+            >
+              Delete
+            </span>
+          </div>
+        </div>
+
+        <DeliveryOptions
+          cartitem={cartitem}
+          deliveryoptions={deliveryoptions}
+          loadCart={loadCart}
+        />
+      </div>
     </>
-  )
+  );
 }
